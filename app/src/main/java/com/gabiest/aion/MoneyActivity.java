54d1963a4768b4dc20f4a0899d4682d0a1ba37d7
rhtjsdud1986 <ssunyoung1984@gmail.com>
2020-06-23 13:49:08 +0900

    new initial commit 0623 1

--- app/src/main/java/com/gabiest/aion/MoneyActivity.java
+++ app/src/main/java/com/gabiest/aion/MoneyActivity.java
@@ -5,7 +5,7 @@ import android.os.Bundle;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MoneyActivity extends AppCompatActivity {
-    
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
